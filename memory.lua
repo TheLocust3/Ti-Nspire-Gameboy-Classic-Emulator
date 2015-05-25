69f1b1eeb24dd1da954824eed6f819a39ef40db6
Jake Kinsella <jake.kinsella@gmail.com>
2015-05-25 10:53:44 -0400

    Add functions for memory

--- memory.lua
+++ memory.lua
@@ -1,15 +1,16 @@
 function get_8b (location)
-
+	return memory[location]
 end
 
 function get_16b (location)
-
+	return to16b(location, location + 1)
 end
 
-function write_8b (location)
-
+function write_8b (location, value)
+	memory[location] = value
 end
 
-function write_16b (location)
-
+function write_16b (location, value)
+	memory[location] = shiftRight(value, 8)
+	memory[location] = bitwiseAnd(toBits(value, 16), 0x00ff)
 end
