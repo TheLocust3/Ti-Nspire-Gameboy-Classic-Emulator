6b0f50a8704da01b6e8171327119c77963bb5834
Jake Kinsella <jake.kinsella@gmail.com>
2015-05-28 14:52:02 -0400

    Add hex output for calculator

--- calcTest.lua
+++ calcTest.lua
@@ -1,6 +1,6 @@
 function on.paint (gc)
-	gc:drawString("A: " .. registers[1] .. " B: " .. registers[2] .. " C: " .. registers[3] .. " D: " .. registers[4], 2, 10)
-	gc:drawString(" E: " .. registers[5] .. " F: " .. registers[8] .. " H: " .. registers[6] .. " L: " .. registers[7] .. " PC: " .. registers[9], 2, 30)
+	gc:drawString("A: " .. toHex(registers[1]) .. " B: " .. toHex(registers[2]) .. " C: " .. toHex(registers[3]) .. " D: " .. toHex(registers[4]), 2, 10)
+	gc:drawString(" E: " .. toHex(registers[5]) .. " F: " .. toHex(registers[8]) .. " H: " .. toHex(registers[6]) .. " L: " .. toHex(registers[7]) .. " PC: " .. toHex(registers[9]), 2, 30)
 	gc:drawString(command, 2, 50)
 end
 
