ef16db173f638500ece17d985368db6762d4257e
Jake Kinsella <jake.kinsella@gmail.com>
2015-06-01 21:43:42 -0400

    Add DIV register to graphic

--- calcTest.lua
+++ calcTest.lua
@@ -1,6 +1,7 @@
 function on.paint (gc)
 	gc:drawString("A: " .. toHex(registers[1]) .. " B: " .. toHex(registers[2]) .. " C: " .. toHex(registers[3]) .. " D: " .. toHex(registers[4]), 2, 10)
-	gc:drawString(" E: " .. toHex(registers[5]) .. " F: " .. toHex(registers[8]) .. " H: " .. toHex(registers[6]) .. " L: " .. toHex(registers[7]) .. " PC: " .. toHex(registers[9]), 2, 30)
+	gc:drawString("E: " .. toHex(registers[5]) .. " F: " .. toHex(registers[8]) .. " H: " .. toHex(registers[6]) .. " L: " .. toHex(registers[7]) .. " PC: " .. toHex(registers[9]), 2, 30)
 	gc:drawString(command, 2, 50)
-	gc:drawString(time .. " ms", 2, 70)
+	gc:drawString("Timer Loop: " .. time .. " ms", 2, 70)
+	gc:drawString("DIV Register: " .. toHex(get_8b(0xff04)), 2, 90)
 end
