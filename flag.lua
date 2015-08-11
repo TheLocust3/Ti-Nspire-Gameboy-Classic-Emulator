b88bf95716c8eed2cb8349f47d5b17d23f5ccd9a
Jake Kinsella <jake.kinsella@gmail.com>
2015-08-11 16:13:26 -0400

    Add central methods for flag classes

--- flag.lua
+++ flag.lua
@@ -16,8 +16,44 @@ end
 
 ZeroFlag = class(Flag) 
 
+function ZeroFlag:isZero(number)
+  return number == 0
+end
+
 SubtractFlag = class(Flag)
 
 CarryFlag = class(Flag)
 
+function CarryFlag:isCarry_8b(number)
+  return number > 0xff
+end
+
+function CarryFlag:isCarry_16b(number)
+  return number > 0xffff
+end
+
 HalfCarryFlag = class(Flag)
+
+function HalfCarryFlag:isHalfCarryAdd_8b (a, b)
+  if bitwiseAnd_8(bitwiseAnd_8(a, 0xf) + bitwiseAnd_8(b, 0xf), 0xf0) > 0 then
+    return true
+  else
+    return false
+  end
+end
+
+function HalfCarryFlag:isHhalfCarryAdd_16b (a, b)
+  if bitwiseAnd_16(bitwiseAnd_16(a, 0xfff) + bitwiseAnd_16(b, 0xfff), 0xfff0) > 0 then
+    return true
+  else
+    return false
+  end
+end
+
+function HalfCarryFlag:isHalfCarrySub_8b (a, b)
+  if bitwiseAnd_8(bitwiseAnd_8(a, 0xf) - bitwiseAnd_8(b, 0xf), 0x08) ~= a then
+    return true
+  else
+    return false
+  end
+end
